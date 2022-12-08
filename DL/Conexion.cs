28d0f1b0599e9bd80c26bc0f988f6e0f670dfaa5
PerezAlexia <alexia.deyanin@gmail.com>
2022-12-08 14:58:09 -0600

    Agregar archivos de proyecto.

---
+++ DL/Conexion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class Conexion 
+    {
+        public static string GetConnectionString()
+        {
+            return ConfigurationManager.ConnectionStrings["JJuarezPruebaTecnica"].ConnectionString.ToString();
+        }
+
+        private static SqlConnection SqlConnection(Func<string> getConnectionString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ML/Libro.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ML
+{
+    public class Libro
+    { 
+        public string IdLibro { get; set; }
+        public string Nombre { get; set; } 
+
+        public int NumeroPaginas { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public string Edicion { get; set; }
+
+        // Propiedades de navegacion 
+
+        // AUTOR 
+        public ML.Autor Autor { get; set; }
+
+        //EDITORIAL 
+        public ML.Editorial Editorial { get; set; }
+
+        //GENERO  
+        public ML.Genero Genero { get; set; }
+
+    }
+}
